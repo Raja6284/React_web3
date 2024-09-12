5320ced280752b581853ed9e51fd08025e026a15
Raja kumar <rajaraja360parth@gmail.com>
2024-09-12 19:49:44 +0530

    useState to generate card

--- part1/src/component/Card.jsx
+++ part1/src/component/Card.jsx
@@ -1,33 +1,57 @@
 import React from 'react'
 import './Card.css'
+import { useState } from 'react'
+
+
 function Card(){
 
-    const Cards = [{title:"Title 1",owner:"owner 1"},
-                {title:"Title 1",owner:"owner 1"},
-                {title:"Title 1",owner:"owner 1"},
-                {title:"Title 1",owner:"owner 1"},
-                {title:"Title 1",owner:"owner 1"}
-    ]
+    const[Cards,setCard] = useState([{title:"Title: 1",owner:"Owner: 1"}])
+    // const Cards = [{title:"Title 1",owner:"owner 1"},
+    //             {title:"Title 1",owner:"owner 1"},
+    //             {title:"Title 1",owner:"owner 1"},
+    //             {title:"Title 1",owner:"owner 1"},
+    //             {title:"Title 1",owner:"owner 1"}
+    // ]
 
     const CardGenerator = ()=>{
-        Cards.push({title:"Title 1",owner:"owner 1"})
+        // Cards.push({title:"Title 1",owner:"owner 1"})
+        const newObject = {title:`Title:${Cards.length+1}`,owner:`Owner: Raja ${Cards.length+1}`}
+        setCard([...Cards,newObject])
         console.log("card pushed to container")
     }
 
+    const[counter,setCounter] = useState(0)
+    //let counter = 0
+    
+    const increment=()=>{
+        //counter++;
+        setCounter(counter+1)
+        console.log("counter:",counter)
+    }
+    console.log("render")
     return(
         <>
+        <button className = 'generate-btn m-2' onClick={CardGenerator}>Generate Card</button>
         <div className='card-container'>
         {
-            Cards.map((Card)=>{
+            Cards.map((Card,index)=>{
                 return(
-                    <div className='card'><h3>{Card.title}<br/>{Card.owner}</h3></div>
+                    <div className='card'> key={index}<h3>{Card.title}<br/>{Card.owner}</h3></div>
                 )
             })
          }
          
         </div>
 
-        <button className = 'generate-btn' onClick={CardGenerator}>Generate Card</button>
+         <div className='card-container m-4' >
+            <div className='card'>
+                {counter}
+            </div>
+            
+         </div>
+         <button className = 'generate-btn' onClick={increment}>Increment</button>
+
+         
         </>
         
     )
