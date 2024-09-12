ba4456f97c0ebe23d7c3f9815beeffced42a7220
Raja kumar <rajaraja360parth@gmail.com>
2024-09-12 18:10:52 +0530

    add genrate card button

--- part1/src/component/Card.jsx
+++ part1/src/component/Card.jsx
@@ -9,16 +9,27 @@ function Card(){
                 {title:"Title 1",owner:"owner 1"}
     ]
 
+    const CardGenerator = ()=>{
+        Cards.push({title:"Title 1",owner:"owner 1"})
+        console.log("card pushed to container")
+    }
+
     return(
+        <>
         <div className='card-container'>
-           {
+        {
             Cards.map((Card)=>{
                 return(
                     <div className='card'><h3>{Card.title}<br/>{Card.owner}</h3></div>
                 )
             })
-           }
+         }
+         
         </div>
+
+        <button className = 'generate-btn' onClick={CardGenerator}>Generate Card</button>
+        </>
+        
     )
 }
 
